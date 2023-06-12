cd1a2d8e0fd7f2477475a4498155638e816831b9
neumanlyu <neumannlyu@outlook.com>
2023-06-12 15:11:55 +0800

    add: integer conversion

--- pkg/tool/calculator.go
+++ pkg/tool/calculator.go
@@ -6,8 +6,14 @@ import (
     "strings"
 )
 
-const LittleEndian = 0
-const BigEndian = 1
+const (
+    LITTLE_ENDIAN = 0
+    BIG_ENDIAN    = 1
+    BINARY        = 2
+    OCTAL         = 8
+    DECIMAL       = 10
+    HEXADECIMAL   = 16
+)
 
 // Convert uint32 value to ram arrangement. Using litter endian.
 // @param value: uint32 value
@@ -15,9 +21,9 @@ const BigEndian = 1
 func uint32RamLayout(value uint32, endian int) string {
     // convert uint32 values to byte arrangement(using litter endian)
     data := make([]byte, 4)
-    if endian == LittleEndian {
+    if endian == LITTLE_ENDIAN {
         binary.LittleEndian.PutUint32(data, value)
-    } else if endian == BigEndian {
+    } else if endian == BIG_ENDIAN {
         binary.BigEndian.PutUint32(data, value)
     }
 
@@ -28,4 +34,38 @@ func uint32RamLayout(value uint32, endian int) string {
     }
     ram = strings.TrimSpace(ram)
     return ram
+}
+
+// Integer Conversion
+// @value integer number value
+// @base base number
+// @zeroPadding whether padding using zero
+// @return string of after converted
+func uint32Convert(value uint32, base int, zeroPadding bool) string {
+    if base == BINARY {
+        if zeroPadding {
+            return fmt.Sprintf("%032b", value)
+        } else {
+            return fmt.Sprintf("%b", value)
+        }
+    } else if base == OCTAL {
+        if zeroPadding {
+            return fmt.Sprintf("%011o", value)
+        } else {
+            return fmt.Sprintf("%o", value)
+        }
+    } else if base == DECIMAL {
+        if zeroPadding {
+            return fmt.Sprintf("%010d", value)
+        } else {
+            return fmt.Sprintf("%d", value)
+        }
+    } else if base == HEXADECIMAL {
+        if zeroPadding {
+            return fmt.Sprintf("%08X", value)
+        } else {
+            return fmt.Sprintf("%X", value)
+        }
+    }
+    return ""
 }
--- pkg/tool/calculator_test.go
+++ pkg/tool/calculator_test.go
@@ -6,5 +6,6 @@ import (
 )
 
 func TestUint32ToRamLittleEndian(t *testing.T) {
-    fmt.Printf("uint32ToRamLittleEndian(999): %v\n", uint32RamLayout(999, BigEndian))
+    fmt.Printf("uint32ToRamLittleEndian(999): %v\n", uint32RamLayout(999, LITTLE_ENDIAN))
+    fmt.Printf("uint32Convert(999, DECIMAL): %v\n", uint32Convert(0o1111, BINARY, false))
 }
