1a8e06fe668875b6954149cc69ef7cc4afaabef9
neumanlyu <neumannlyu@outlook.com>
2023-06-13 12:58:23 +0800

    add: 4 bytes complement code(neg)

--- pkg/tool/calculator.go
+++ pkg/tool/calculator.go
@@ -18,7 +18,7 @@ const (
 // Convert uint32 value to ram arrangement. Using litter endian.
 // @param value: uint32 value
 // @return string of ram arrangement
-func uint32RamLayout(value uint32, endian int) string {
+func ramLayoutUint32(value uint32, endian int) string {
     // convert uint32 values to byte arrangement(using litter endian)
     data := make([]byte, 4)
     if endian == LITTLE_ENDIAN {
@@ -36,12 +36,12 @@ func uint32RamLayout(value uint32, endian int) string {
     return ram
 }
 
-// Integer Conversion
+// Integer Conversion following base
 // @value integer number value
 // @base base number
 // @zeroPadding whether padding using zero
 // @return string of after converted
-func uint32Convert(value uint32, base int, zeroPadding bool) string {
+func baseConvertUint32(value uint32, base int, zeroPadding bool) string {
     if base == BINARY {
         if zeroPadding {
             return fmt.Sprintf("%032b", value)
@@ -69,3 +69,24 @@ func uint32Convert(value uint32, base int, zeroPadding bool) string {
     }
     return ""
 }
+
+// calculate complement code. invert and add one
+// @v value
+// @base base
+// @return string of complement code
+func neg32(v int32, base int) string {
+    bits := binary.Size(v) * 8
+    if v >= 0 {
+        // the complememt of a positive number is equal to the original code
+        return baseConvertUint32(uint32(v), base, true)
+    } else {
+        // 1 << uint(32) set the highest to 1 and the rest to 0.
+        // 100000000000000000000000000000000 （1 + 32 zeros） The maximum representation range.
+        // The complement of negative numbers = maximum representation range - the positive value
+        // The complement code = maximum representaion range + value.
+        complementCode := (1 << bits) + v // 按位取反再加 1 得到补码, 最大表示范围-x
+        return baseConvertUint32(uint32(complementCode), base, true)
+    }
+}
+
+// 给内存值，计算真值
--- pkg/tool/calculator_test.go
+++ pkg/tool/calculator_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 func TestUint32ToRamLittleEndian(t *testing.T) {
-    fmt.Printf("uint32ToRamLittleEndian(999): %v\n", uint32RamLayout(999, LITTLE_ENDIAN))
-    fmt.Printf("uint32Convert(999, DECIMAL): %v\n", uint32Convert(0o1111, BINARY, false))
+    // fmt.Printf("uint32ToRamLittleEndian(999): %v\n", ramLayoutUint32(52, LITTLE_ENDIAN))
+    // fmt.Printf("uint32Convert(999, DECIMAL): %v\n", numConvertUint32(0o1111, BINARY, false))
+    fmt.Printf("neg(100, BINARY, true): %v\n", neg32(-128, BINARY))
 }
