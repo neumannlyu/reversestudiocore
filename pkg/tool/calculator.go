5e8672231eb3283df60f7f8e2f172094875c5f4c
neumanlyu <neumannlyu@outlook.com>
2023-06-13 15:18:57 +0800

    add: convert ram date to float

--- pkg/tool/calculator.go
+++ pkg/tool/calculator.go
@@ -2,38 +2,88 @@ package tool
 
 import (
     "encoding/binary"
+    "errors"
     "fmt"
+    "math"
     "strings"
 )
 
 const (
-    LITTLE_ENDIAN = 0
-    BIG_ENDIAN    = 1
-    BINARY        = 2
-    OCTAL         = 8
-    DECIMAL       = 10
-    HEXADECIMAL   = 16
+    LITTLE_ENDIAN  = 0
+    BIG_ENDIAN     = 1
+    BINARY         = 2
+    OCTAL          = 8
+    DECIMAL        = 10
+    HEXADECIMAL    = 16
+    UseBaseType    = "v is a base type like int float"
+    UnknownVarSize = "size of var is not 32 or 64"
 )
 
 // Convert uint32 value to ram arrangement. Using litter endian.
 // @param value: uint32 value
 // @return string of ram arrangement
-func ramLayoutUint32(value uint32, endian int) string {
-    // convert uint32 values to byte arrangement(using litter endian)
-    data := make([]byte, 4)
-    if endian == LITTLE_ENDIAN {
-        binary.LittleEndian.PutUint32(data, value)
-    } else if endian == BIG_ENDIAN {
-        binary.BigEndian.PutUint32(data, value)
+func ramLayout(v interface{}, endian int) (string, error) {
+    bytes := binary.Size(v)
+    if bytes < 0 {
+        return "", errors.New(UseBaseType)
     }
 
-    // ram arrangement
-    var ram string
-    for _, d := range data {
-        ram += fmt.Sprintf("%02X ", d)
+    // convert uint32 values to byte arrangement(using litter endian)
+    if bytes == 4 {
+        data := make([]byte, 4)
+        if endian == LITTLE_ENDIAN {
+            binary.LittleEndian.PutUint32(data, v.(uint32))
+        } else if endian == BIG_ENDIAN {
+            binary.BigEndian.PutUint32(data, v.(uint32))
+        }
+        // ram arrangement
+        var ram string
+        for _, d := range data {
+            ram += fmt.Sprintf("%02X ", d)
+        }
+        ram = strings.TrimSpace(ram)
+        return ram, nil
+    } else if bytes == 8 {
+        data := make([]byte, 8)
+        if endian == LITTLE_ENDIAN {
+            binary.LittleEndian.PutUint64(data, v.(uint64))
+        } else if endian == BIG_ENDIAN {
+            binary.BigEndian.PutUint64(data, v.(uint64))
+        }
+        // ram arrangement
+        var ram string
+        for _, d := range data {
+            ram += fmt.Sprintf("%02X ", d)
+        }
+        ram = strings.TrimSpace(ram)
+        return ram, nil
     }
-    ram = strings.TrimSpace(ram)
-    return ram
+
+    return "", errors.New(UnknownVarSize)
+}
+
+// ram data(4 or 8 bytes) convert to float data
+// @ram the data of ram
+// @endian little or big endian
+// @return interface{} float32 or float64
+func ramData2Float(ram []byte, endian int) (interface{}, error) {
+    if len(ram) != 4 && len(ram) != 8 {
+        return -1, errors.New(UnknownVarSize)
+    }
+    if len(ram) == 4 {
+        if endian == LITTLE_ENDIAN {
+            return math.Float32frombits(binary.LittleEndian.Uint32(ram)), nil
+        } else if endian == BIG_ENDIAN {
+            return math.Float32frombits(binary.BigEndian.Uint32(ram)), nil
+        }
+    } else if len(ram) == 8 {
+        if endian == LITTLE_ENDIAN {
+            return math.Float64frombits(binary.LittleEndian.Uint64(ram)), nil
+        } else if endian == BIG_ENDIAN {
+            return math.Float64frombits(binary.BigEndian.Uint64(ram)), nil
+        }
+    }
+    return "", errors.New(UnknownVarSize)
 }
 
 // Integer Conversion following base
@@ -41,52 +91,74 @@ func ramLayoutUint32(value uint32, endian int) string {
 // @base base number
 // @zeroPadding whether padding using zero
 // @return string of after converted
-func baseConvertUint32(value uint32, base int, zeroPadding bool) string {
-    if base == BINARY {
-        if zeroPadding {
-            return fmt.Sprintf("%032b", value)
-        } else {
-            return fmt.Sprintf("%b", value)
-        }
-    } else if base == OCTAL {
-        if zeroPadding {
-            return fmt.Sprintf("%011o", value)
-        } else {
-            return fmt.Sprintf("%o", value)
-        }
-    } else if base == DECIMAL {
-        if zeroPadding {
-            return fmt.Sprintf("%010d", value)
-        } else {
-            return fmt.Sprintf("%d", value)
-        }
-    } else if base == HEXADECIMAL {
-        if zeroPadding {
-            return fmt.Sprintf("%08X", value)
-        } else {
-            return fmt.Sprintf("%X", value)
-        }
+func baseConvert(v interface{}, base int) (string, error) {
+    bits := binary.Size(v) * 8
+    if bits < 0 {
+        return "", errors.New(UseBaseType)
     }
-    return ""
+
+    switch base {
+    case BINARY:
+        return fmt.Sprintf("%0*b", bits, v), nil
+    case OCTAL:
+        return fmt.Sprintf("%0*o", bits/3+1, v), nil
+    case DECIMAL:
+        return fmt.Sprintf("%d", v), nil
+    case HEXADECIMAL:
+        return fmt.Sprintf("%0*X", bits/4, v), nil
+    }
+
+    return "", errors.New("Unknown base")
+}
+
+// float convert to binary
+func baseConvertFloat(v interface{}) (interface{}, error) {
+    bits := binary.Size(v) * 8
+    if bits < 0 {
+        return nil, errors.New(UseBaseType)
+    }
+    if bits == 32 {
+        return math.Float32bits(v.(float32)), nil
+    } else if bits == 64 {
+        return math.Float64bits(v.(float64)), nil
+    }
+    return nil, errors.New(UnknownVarSize)
 }
 
 // calculate complement code. invert and add one
 // @v value
 // @base base
 // @return string of complement code
-func neg32(v int32, base int) string {
+func neg(v interface{}) (interface{}, error) {
     bits := binary.Size(v) * 8
-    if v >= 0 {
-        // the complememt of a positive number is equal to the original code
-        return baseConvertUint32(uint32(v), base, true)
-    } else {
-        // 1 << uint(32) set the highest to 1 and the rest to 0.
-        // 100000000000000000000000000000000 （1 + 32 zeros） The maximum representation range.
-        // The complement of negative numbers = maximum representation range - the positive value
-        // The complement code = maximum representaion range + value.
-        complementCode := (1 << bits) + v // 按位取反再加 1 得到补码, 最大表示范围-x
-        return baseConvertUint32(uint32(complementCode), base, true)
+    if bits < 0 {
+        return nil, errors.New(UseBaseType)
     }
-}
 
-// 给内存值，计算真值
+    if bits == 32 {
+        if v.(int32) >= 0 {
+            // the complememt of a positive number is equal to the original code
+            return v.(uint32), nil
+        } else {
+            // 1 << uint(32) set the highest to 1 and the rest to 0.
+            // 100000000000000000000000000000000 （1 + 32 zeros） The maximum representation range.
+            // The complement of negative numbers = maximum representation range - the positive value
+            // The complement code = maximum representaion range + value.
+            complementCode := uint32((1 << bits) + v.(int32))
+            return complementCode, nil
+        }
+    } else if bits == 64 {
+        if v.(int64) >= 0 {
+            // the complememt of a positive number is equal to the original code
+            return v.(uint64), nil
+        } else {
+            // 1 << uint(32) set the highest to 1 and the rest to 0.
+            // 100000000000000000000000000000000 （1 + 32 zeros） The maximum representation range.
+            // The complement of negative numbers = maximum representation range - the positive value
+            // The complement code = maximum representaion range + value.
+            complementCode := (uint64)((1 << bits) + v.(int64))
+            return complementCode, nil
+        }
+    }
+    return nil, errors.New(UnknownVarSize)
+}
--- pkg/tool/calculator_test.go
+++ pkg/tool/calculator_test.go
@@ -7,6 +7,41 @@ import (
 
 func TestUint32ToRamLittleEndian(t *testing.T) {
     // fmt.Printf("uint32ToRamLittleEndian(999): %v\n", ramLayoutUint32(52, LITTLE_ENDIAN))
-    // fmt.Printf("uint32Convert(999, DECIMAL): %v\n", numConvertUint32(0o1111, BINARY, false))
-    fmt.Printf("neg(100, BINARY, true): %v\n", neg32(-128, BINARY))
+
+    // test read ram to float
+
+    if v, err := ramData2Float([]byte{0x00, 0x68, 0x45, 0x44}, LITTLE_ENDIAN); err != nil {
+        fmt.Printf("err.Error(): %v\n", err.Error())
+    } else {
+        fmt.Printf("v: %v\n", v)
+    }
+
+    // fmt.Printf("uint32Convert(999, DECIMAL): %v\n", baseConvertUint32(int32(100), BINARY))
+
+    // test neg
+    /*
+       if v, err := neg(int32(-100)); err != nil {
+           fmt.Printf("err.Error(): %v\n", err.Error())
+       } else {
+           if s, err := baseConvert(v, BINARY); err != nil {
+               fmt.Printf("err.Error(): %v\n", err.Error())
+           } else {
+               fmt.Printf("s: %v\n", s)
+           }
+       }
+    */
+
+    // test convert float
+    /*
+       if v, err := baseConvertFloat(float32(895.75)); err != nil {
+           fmt.Printf("err.Error(): %v\n", err.Error())
+       } else {
+           if s, err := ramLayout(v, LITTLE_ENDIAN); err != nil {
+               fmt.Printf("err.Error(): %v\n", err.Error())
+           } else {
+               fmt.Printf("s: %v\n", s)
+           }
+           // fmt.Printf("v: %v\n", v)
+       }
+    */
 }
